df0bb138ae2a8c80f4d4976b71ea8548aa787861
Kerby Shedden <kshedden@umich.edu>
2020-01-20 15:11:06 -0500

    update

--- julia_intro/julia/ais_simple.jl
+++ julia_intro/julia/ais_simple.jl
@@ -20,29 +20,35 @@
 # packages using `Pkg.add`.
 
 # Package imports
+
 using DataFrames, CSV, Clustering, StatsBase, GZip, Printf
 
 # Read the data.  This code is included in another script so that it
 # can be re-used elsewhere.
+
 include("read_ais_data.jl")
 
 # Count the number of unique vessels.  DataFrame columns can be accessed
 # with the dot syntax used here for brevity.
+
 n_vessels = size(unique(df.VesselName))
 
 # This is equivalent to above, using a different syntax to access the
 # dataframe column.  A token of the form :x is a called a "symbol".
 # If you want to know more about symbols in julia, see here:
 # https://stackoverflow.com/a/23482257/1941745
+
 n_vessels = size(unique(df[:, :VesselName]))
 
 # Look at the distribution of records over the status codes.  countmap
 # is in StatsBase, which we imported above.
+
 cm = countmap(df.Status)
 
 # Get the maximum speed per vessel.  We write a custom aggregation
 # function to handle missing values, and the special case where all
 # values in a group are missing.
+
 function g1(x)
     if count(ismissing, x) == length(x)
         return missing
@@ -55,42 +61,52 @@ maxspeed1 = by(df, :VesselName, :SOG=>g1)
 # This is equivalent to the preceeding cell, expressed more concisely.
 # The syntax "x -> expression" creates an anonymous function that evaluates
 # the expression using a provided value of x.
+
 g2 = x -> count(ismissing, x) == length(x) ? missing : maximum(skipmissing(x))
 maxspeed2 = by(df, :VesselName, :SOG=>g2)
 
 # Confirm that the results are the same
+
 nx = sum(skipmissing(maxspeed1[:, 2] .!= maxspeed2[:, 2]))
 
 # Create a dataframe containing records for moored cargo vessels.
+
 dx = filter(x -> !ismissing(x.Status) && x.Status == "moored", df)
 dx = filter(x -> !ismissing(x.VesselType) && x.VesselType in [70, 71, 73, 74, 76], dx)
 
 # We will do a cluster analysis to find where the moored cargo ships are located.
 # This clustering is based only on the spatial position of each vessel, so we
 # restrict to those variables here.
+
 dd = dx[:, [:LAT, :LON]]
 
 # A few of the records have missing position information, which the clustering
 # algorithm can't easily handle.  So we drop those records here.
+
 dd = dd[completecases(dd), :]
 
 # The clustering code wants the positions in the form of a matrix, not a dataframe,
 # so we do the conversion here.
+
 dd = Matrix{Float64}(dd)
 
 # The clustering algorithm wants the objects in the columns and the variables
 # (coordinates) in the rows, so we transpose.
+
 dd = transpose(dd)
 
 # Use k-means clustering to locate some clusters of moored cargo ships
+
 cl = kmeans(dd, 5)
 
 # Get the sizes of the clusters and figure out how to sort the clusters
 # by descending size.
+
 ct = counts(cl)
 ii = sortperm(ct, rev=true)
 
 # Create urls that can be used to locate the clusters on a map.
+
 pos = cl.centers
 for i in ii
     @printf("https://www.google.com/maps/search/?api=1&query=%f,%f\n", pos[1, i], pos[2, i])
--- julia_intro/julia/read_ais_data.jl
+++ julia_intro/julia/read_ais_data.jl
@@ -13,28 +13,35 @@
 # packages using `Pkg.add`.
 
 # Package imports
+
 using DataFrames, CSV, GZip
 
 # This variable should be set to the same value as in the get_ais_data.jl
 # script.
+
 data_dir = "/nfs/kshedden/AIS"
 
 # The year for the data file to be analyzed, as a string.
+
 data_year = "2016"
 
 # The name of the data file to be analyzed.
+
 data_file = "AIS_2016_01_Zone10.csv.gz"
 
 # A directory name baked into the zip archives, do not change.
+
 ais_dir = "AIS_ASCII_by_UTM_Month"
 
 # The full path to the data file
+
 fn = joinpath(data_dir, "raw", ais_dir, data_year, data_file)
 
 # Specify column types for certain key variables.  The symbol => in Julia
 # is called the "Pair" function.  Writing x=>y and Pair(x, y) are equivalent.
 # The pair function is used whenever it is natural to match two values in
 # an ordered pair.
+
 ty = Dict("LAT"=>Float64, "LON"=>Float64, "HEADING"=>Float64, "Length"=>Float64,
           "Width"=>Float64, "Draft"=>Float64, "SOG"=>Float64, "COG"=>Float64,
           "VesselType"=>Int64)
@@ -44,6 +51,7 @@ ty = Dict("LAT"=>Float64, "LON"=>Float64, "HEADING"=>Float64, "Length"=>Float64,
 # variable name has to be defined as something (anything) outside the
 # block, otherwise the df variable is local to the block and cannot
 # be used after the block closes.
+
 df = nothing
 GZip.open(fn) do f
     global df
