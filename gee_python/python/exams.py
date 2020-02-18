e94533af96fc526efbf51a2f4d9f2c1cbf2697e3
Kerby Shedden <kshedden@umich.edu>
2020-02-17 19:21:50 -0500

    update

--- gee_python/python/exams.py
+++ gee_python/python/exams.py
@@ -2,8 +2,16 @@ import pandas as pd
 import numpy as np
 import statsmodels.api as sm
 
-# Exam scores data from this page:
+# # GEE analysis of test score data
+
+# Generalized estimating equations (GEE) can be used to analyze
+# multilevel data that arises in educational research, for example
+# when students taking a test are grouped into classrooms.
+
+# The examination data analyzed here are obtained from this page:
 # http://www.bristol.ac.uk/cmm/learning/support/datasets/
+
+# The data are in fixed-width format, we can load it as follows:
 
 # +
 colspecs = [(0, 5), (6, 10), (11, 12), (13, 16), (17, 20)]
@@ -13,6 +21,9 @@ df["female"] = 1*(df.gender == 1)
 df = df.dropna()
 # -
 
+# Here is a basic model looking at the scores on exam 1 by gender, using
+# the default independence working correlation structure.
+
 # +
 # A school-clustered model for exam score 1 with no correlation.
 model1 = sm.GEE.from_formula("score1 ~ female", groups="schoolid", data=df)
@@ -20,21 +31,25 @@ rslt1 = model1.fit()
 print(rslt1.summary())
 # -
 
+# Here is the same mean structure model, now specifying that the students
+# are exchangeably correlated within classrooms.
+
 # +
 # A school-clustered model for exam score 1 with exchangeable correlations.
 model2 = sm.GEE.from_formula("score1 ~ female", groups="schoolid",
                              cov_struct=sm.cov_struct.Exchangeable(), data=df)
 rslt2 = model2.fit()
 print(rslt2.summary())
+print(model2.cov_struct.summary())
 # -
 
-# +
-# A subject-clustered model for exam score 1 with exchangeable correlations.
-model3 = sm.GEE.from_formula("score1 ~ female", groups="subjectid",
-                             cov_struct=sm.cov_struct.Exchangeable(), data=df)
-rslt3 = model3.fit()
-print(rslt3.summary())
-# -
+# Next we will pivot the exam scores so that each subject has two observations
+# on a single "test" variable (one observation for the first test and one for
+# the second test).  This is a form of repeated measures, but since the tests
+# are different, we also include a covariate indicating which test is being
+# recorded.  We now have two levels of repeated structure: two test scores per
+# student, and multiple students per classroom.  We can use a nested correlation
+# structure to estimate the variance contributions from the two levels.
 
 # +
 # Prepare to do a joint analysis of the two scores.
@@ -45,8 +60,9 @@ dx = pd.melt(df, id_vars=["subjectid", "schoolid", "female"],
 
 # +
 # A nested model for subjects within schools, having two scores per subject.
-model4 = sm.GEE.from_formula("score ~ female + test", groups="schoolid", dep_data="0 + subjectid",
+model3 = sm.GEE.from_formula("score ~ female + test", groups="schoolid", dep_data="0 + subjectid",
                              cov_struct=sm.cov_struct.Nested(), data=dx)
-rslt4 = model4.fit()
-print(rslt4.summary())
+rslt3 = model3.fit()
+print(rslt3.summary())
+print(model3.cov_struct.summary())
 # -
